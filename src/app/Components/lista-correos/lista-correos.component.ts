682e8d06eae83b604ce4b6d922cfbf340e63d43f
jeni <jeni>
2020-03-25 17:49:50 +0100

    Cremos el listado de correos usando ngFor

--- src/app/Components/lista-correos/lista-correos.component.ts
+++ src/app/Components/lista-correos/lista-correos.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaCorreosComponent implements OnInit {
 
-  constructor() { }
+  listaCorreos: any[];
+
+  constructor() { 
+
+    const correo1 = {
+      asunto: "Prueba primer correo",
+      cuerpo: "Este es un correo de prueba para la segunda práctica del curso de Angular que consiste en mostrar un listado de correos",
+      emisor: "jeni@prueba.es",
+      receptor: "lucia@prueba.es"
+    }
+
+    const correo2 = {
+      asunto: "Prueba segundo correo",
+      cuerpo: "Este es el segundo correo de prueba para la práctica de angular",
+      emisor: "jeni@prueba.es",
+      receptor: "josua@prueba.es"
+    }
+
+    this.listaCorreos = [];
+    this.listaCorreos.push(correo1);
+    this.listaCorreos.push(correo2);
+
+  }
 
   ngOnInit(): void {
   }
