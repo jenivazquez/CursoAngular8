02ce5a831940653f9244443f333383dc4eaf18a5
jeni <jeni>
2020-03-26 09:40:15 +0100

    Alta de correo con formulario reactivo

--- src/app/nuevo-correo/nuevo-correo.component.ts
+++ src/app/nuevo-correo/nuevo-correo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-correo',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoCorreoComponent implements OnInit {
 
-  constructor() { }
+  nuevoCorreo: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.nuevoCorreo = this.formBuilder.group({
+      asunto: ['', [Validators.required, Validators.maxLength(20)]],
+      cuerpo: ['', [Validators.required, Validators.maxLength(100)]],
+      receptor: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  get formulario() {
+    return this.nuevoCorreo.controls;
+  }
+
+  onSubmit(): void {
+
+    this.submitted = true;
+
+    if (this.nuevoCorreo.invalid) {
+      return;
+    }
+
+    let correo = this.nuevoCorreo.value;
+    correo.leido = false;
+    correo.emisor = 'jeni@prueba.es';
+
+    alert('El correo ha sido enviado. Eliminamos el formulario.');
+    this.onReset();
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.nuevoCorreo.reset();
   }
 
 }
