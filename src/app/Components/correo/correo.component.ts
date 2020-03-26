289d25ae1fb2e15f90b951f2ce4fa926092d80f0
jeni <jeni>
2020-03-26 08:33:56 +0100

    Creación del componente nuevo-correo

--- src/app/Components/correo/correo.component.ts
+++ src/app/Components/correo/correo.component.ts
@@ -9,13 +9,13 @@ export class CorreoComponent implements OnInit {
 
   correo: any;
 
-  constructor() { 
+  constructor() {
     this.correo = {
-      asunto: "Prueba correo",
-      cuerpo: "Este es una correo de prueba para la primera práctica del curso de Angular 8",
-      emisor: "jeni@prueba.es",
-      receptor: "lucia@prueba.es"
-    }
+      asunto: 'Prueba correo',
+      cuerpo: 'Este es una correo de prueba para la primera práctica del curso de Angular 8',
+      emisor: 'jeni@prueba.es',
+      receptor: 'lucia@prueba.es'
+    };
   }
 
   ngOnInit(): void {
